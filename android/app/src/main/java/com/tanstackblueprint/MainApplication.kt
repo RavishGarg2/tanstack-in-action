d076754da97aaa1dd7b61e20f130402aed466d69
Ravish <ravishgarg8@gmail.com>
2026-08-18 11:40:03 +0530

    changed name

--- android/app/src/main/java/com/tanstackblueprint/MainApplication.kt
+++ android/app/src/main/java/com/tanstackblueprint/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.visioncamera
+package com.tanstackblueprint
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/TanStackBlueprint/AppDelegate.swift
+++ ios/TanStackBlueprint/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "VisionCamera",
+      withModuleName: "TanStackBlueprint",
       in: window,
       launchOptions: launchOptions
     )
